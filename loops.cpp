8676cd1566460b15c84fdf78aadc7f40f39053dd
iamkanika <kanikagarg.2707@gmail.com>
2023-04-28 21:12:20 +0530

    Loops

--- loops.cpp
+++ loops.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     int n, sum = 0;
-    cout << "Enter the value of n numbers to display all numbers with their sum =";
+    cout << "Enter the value of numbers to display all numbers with their sum =";
     cin >> n;
     int i = 1;
     // printing numbers from 1 to n//
