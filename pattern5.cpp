1f91e3033906ae986623a8a3f68bb57dc1c29e4d
iamkanika <kanikagarg.2707@gmail.com>
2023-05-13 02:33:44 +0530

    numbers

--- pattern5.cpp
+++ pattern5.cpp
@@ -1,26 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int n;
-    cout<<"Enter the value of n="<<endl;
-    cin>>n;
-    
-    int i=1;
-    int count=1;
-    while (i<=n)
+    cout << "Enter the value of n=" << endl;
+    cin >> n;
+
+    int i = 1;
+    int count = 1;
+    while (i <= n)
     {
-        int j=1;
-        while(j<=n)
+        int j = 1;
+        while (j <= n)
         {
-            
-        cout<< count ;
-        cout<<" ";
-        count=count+1;
-        j=j+1;
+
+            cout << count;
+            cout << " ";
+            count = count + 1;
+            j = j + 1;
         }
-        cout<<endl;
-        i=i+1;
+        cout << endl;
+        i = i + 1;
     }
-    
 }
