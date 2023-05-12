8591a66c1fae4110b94745a4ac8fb53e893b082f
iamkanika <kanikagarg.2707@gmail.com>
2023-05-13 02:34:51 +0530

    Pattern 7

--- pattern7.cpp
+++ pattern7.cpp
@@ -1,22 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-// main
 int main()
 {
     int n;
-    cout<<"Enter the value of n ="<<endl;
-    cin>>n;
-    int i=1;
-    while (i<=n)
+    cout << "Enter the value of n =" << endl;
+    cin >> n;
+    int i = 1;
+    while (i <= n)
     {
-        int j=1;
-        while (j<=i)
+        int j = 1;
+        while (j <= i)
         {
-            cout<<j;
-            j=j+1;
+            cout << j;
+            j = j + 1;
         }
-    
-    cout<<endl;
-    i=i+1;
+
+        cout << endl;
+        i = i + 1;
     }
 }
