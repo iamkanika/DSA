0f1c6d23765c976cc8b7555b055e599046ad12f0
iamkanika <kanikagarg.2707@gmail.com>
2023-05-13 02:35:44 +0530

    Pattern 9

--- pattern9.cpp
+++ pattern9.cpp
@@ -1,26 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
     int n;
-    cout<<" Enter the value of n="<<endl;
-    cin>>n;
-    int i=1;
-    int count=1;
-    while (i<=n)
+    cout << " Enter the value of n=" << endl;
+    cin >> n;
+    int i = 1;
+    int count = 1;
+    while (i <= n)
     {
-        int j=1;
-        while (j<=i)
+        int j = 1;
+        while (j <= i)
         {
-    
-            cout<<count<<" ";
-            count=count+1;
-            j=j+1;
 
+            cout << count << " ";
+            count = count + 1;
+            j = j + 1;
         }
-        cout<<endl;
-        i=i+1;
+        cout << endl;
+        i = i + 1;
     }
-    
 }
