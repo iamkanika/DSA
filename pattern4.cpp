b8c4b23b4e6c09e21fb032a246a66d188771aeac
iamkanika <kanikagarg.2707@gmail.com>
2023-05-13 02:33:14 +0530

    numbers in decreasing mode

--- pattern4.cpp
+++ pattern4.cpp
@@ -1,23 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int n;
-    cout<<"Enter the value of n="<<endl;
-    cin>>n;
-    int i=1;
-    while (i<=n)
+    cout << "Enter the value of n=" << endl;
+    cin >> n;
+    int i = 1;
+    while (i <= n)
     {
-        int j=1;
-        while (j<=n)
+        int j = 1;
+        while (j <= n)
         {
-            cout<<n-j+1;
-            j=j+1;
-
+            cout << n - j + 1;
+            j = j + 1;
         }
-        cout<<endl;
-        i=i+1;
-    
+        cout << endl;
+        i = i + 1;
     }
-    
 }
