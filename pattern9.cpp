90de4dcc136ca4424392d5bf08c777e0580ea9f5
iamkanika <kanikagarg.2707@gmail.com>
2023-05-16 20:12:02 +0530

    Pattern 9

--- pattern9.cpp
+++ pattern9.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     int n;
-    cout << " Enter the value of n = " << endl;
+    cout << " Enter the value of n numbers = " << endl;
     cin >> n;
     int i = 1;
     int count = 1;
